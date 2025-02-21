efb0542aa7223a84d71def6a3963824394aa178d
Châu Minh Quân <104240214@student.vgu.edu.vn>
2025-02-21 09:29:20 +0700

    Rework project structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,27 @@
-from flask import Flask
-from .routes.auth import auth_bp
 
-app = Flask(__name__)
-app.register_blueprint(auth_bp)
+def create_app_with_blueprint():
+    from flask import Flask
+    app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello, World!'
+    # ======================
+    # |    Configuration   |
+    # ======================
+    from .models import db
+    from .config import Config
+    app.config.from_object(Config)
+    db.init_app(app)
 
-def run():
-    app.run(debug=True)
+    # ======================
+    # |    Blueprint       |
+    # ======================
+    from .routes.auth import auth_bp
+    from .routes.api import api_bp
+    app.register_blueprint(auth_bp, url_prefix='/')
+    app.register_blueprint(api_bp, url_prefix='/api')
+
+    # ======================
+    # |    Final stuff     |
+    # ======================
+    with app.app_context():
+        db.create_all()
+    return app
