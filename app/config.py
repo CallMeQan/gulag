0743bc2fa846c905f2945944cdf71a853f19afaf
Châu Minh Quân <104240214@student.vgu.edu.vn>
2025-02-27 13:34:18 +0700

    Set limit file upload size

--- app/config.py
+++ app/config.py
@@ -3,3 +3,4 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///gulag.db'
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
+    MAX_CONTENT_LENGTH = 1024 * 1024 * 1024 # 1GB
