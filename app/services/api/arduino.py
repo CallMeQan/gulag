d123757442c4778928044bb50af3ac0f201f0555
Châu Minh Quân <104240214@student.vgu.edu.vn>
2025-03-08 20:34:11 +0700

    Temp file for arduino.py

--- app/services/api/arduino.py
+++ app/services/api/arduino.py
@@ -2,26 +2,41 @@ import os, json, requests, dotenv
 
 dotenv.load_dotenv()
 
+PASSWORD = os.getenv('PASSWORD')
+EMAIL = os.getenv('EMAIL')
+
 ARDUINO_URL_HISTORY_DATA = 'https://api2.arduino.cc/iot/v2/series/historic_data'
 ARDUINO_AUTH_TOKEN = os.environ.get('ARDUINO_AUTH_TOKEN')
 
 def get_token_file():
     URL = 'https://login.arduino.cc/oauth/token'
     headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
-        'Auth0-Client': 'eyJuYW1lIjoiYXV0aDAtc3BhLWpzIiwidmVyc2lvbiI6IjIuMS4zIn0='
+        "Sec-Ch-Ua-Platform": "\"Windows\"",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Sec-Ch-Ua": "\"Chromium\";v=\"133\", \"Not(A:Brand\";v=\"99\"",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Auth0-Client": "eyJuYW1lIjoiYXV0aDAtc3BhLWpzIiwidmVyc2lvbiI6IjIuMS4zIn0=",
+        "Sec-Ch-Ua-Mobile": "?0",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
+        "Accept": "*/*",
+        "Origin": "https://app.arduino.cc",
+        "Sec-Fetch-Site": "same-site",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Dest": "empty",
+        "Referer": "https://app.arduino.cc/",
+        "Accept-Encoding": "gzip, deflate, br",
     }
-    payload = {
-        'client_id': 'e9qipA2N0k9P8vvre9fdGc6u9Kl9eHSP',
-        'redirect_uri': 'https://app.arduino.cc',
-        'code_verifier': 'vvnrpZM3Ie.JYOablXixaq8Lse0FaKrgZNHVsQRmjv.',
-        'code': 'hP-AFQw0cRIjm1LkcCZEdw-M6JupQG-lcqrVE2V3N3oXx',
-        'grant_type': 'authorization_code'
+
+    data = {
+        "client_id": "e9qipA2N0k9P8vvre9fdGc6u9Kl9eHSP",
+        "redirect_uri": "https://app.arduino.cc",
+        "code_verifier": "yJH8vDqF2X.JsJuINCfp9hU.4q~iKkCpud8DHwbHUGt",
+        "code": "9aleTStEISPxuuDgXhK2seL4P7uSGJtH5ZQBaDcsM9wh-",
+        "grant_type": "authorization_code"
     }
+
     #response = requests.post(URL, headers=headers, data=json.dumps(payload))
-    return requests.options(URL, headers={"access-control-request-headers":
-'auth0-client'}).status_code
+    return requests.post(URL,headers=headers, data=data).status_code
     #return response.ok
 
 def get_history_data():
