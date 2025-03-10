0855e8a8d0d8e9e8201bd5939d4614fbc9efc98c
Châu Minh Quân <104240214@student.vgu.edu.vn>
2025-03-10 14:48:41 +0700

    Fix url_for all template files and remove route login.py

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,11 +1,27 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, jsonify, render_template
 
-app = Blueprint('home', __name__)
+home_bp = Blueprint('home', __name__)
 
-@app.route('/')
+@home_bp.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
-@app.route('/about')
+@home_bp.route('/about')
 def about():
     return render_template('about.html')
+
+@home_bp.route('/login')
+def login():
+    return render_template('login.html')
+
+@home_bp.route('/register', methods=['GET'])
+def register():
+    return render_template('register.html')
+
+@home_bp.route('/register', methods=['POST'])
+def register():
+    return jsonify('djtmem')
+
+@home_bp.route('/forgot_password')
+def forgot_password():
+    return render_template('forgot_password.html')
