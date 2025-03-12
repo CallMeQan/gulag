b18411e90aa7afc2e385e293991553584d8d93af
ceenen <phunh.work@gmail.com>
2025-03-13 01:22:31 +0700

    Finished kinematic data processing

---
+++ app/modules/data_module/__init__.py
@@ -0,0 +1,34 @@
+from .gps_process import read_and_join, process_data, calculate_data
+
+
+
+# if __name__ == "__main__":
+#     data = """
+#     0
+#     220
+#     500
+#     500
+#     200
+#     800
+#     0
+#     550
+#     850
+#     400
+#     78
+#     700
+#     500
+#     550
+#     270
+#     120
+#     400
+#     500
+#     270
+#     300
+#     450
+#     350
+#     7
+#     """
+#     data = data.split()
+#     data = list(map(lambda x: int(x), data))
+#     print(data)
+#     print(sum(data))
