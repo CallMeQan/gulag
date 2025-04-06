be9bcebf0c2a8e20fec4510777de202a9e0a9c9e
w4se <104240581@student.vgu.edu.vn>
2025-04-06 21:03:52 +0700

    finishing testing forget password feature

--- app/modules/forgot_module/forgot_password.py
+++ app/modules/forgot_module/forgot_password.py
@@ -3,12 +3,11 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()  # Load environment variables from .env file
-EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")  # Get the email password from environment variable
 
 # Replace with your own details
 sender_email = "hongp4949@gmail.com"
-receiver_email = "phuhn.a1.2124@gmail.com"
-password = os.getenv("EMAIL_PASSWORD")  # Use an app password if 2FA is enabled
+receiver_email = "cptrekkles@gmail.com"
+password = os.getenv("EMAIL_PASSWORD")  # Use the App Password here
 
 # Email content
 message = """\
@@ -25,11 +24,13 @@ port = 587  # 587 is used for TLS/STARTTLS
 context = ssl.create_default_context()
 
 # Connect to the Gmail SMTP server and send the email
-with smtplib.SMTP(smtp_server, port) as server:
-    server.ehlo()  # Can be used to identify the client to the server
-    server.starttls(context=context)  # Secure the connection using TLS
-    server.ehlo()  # Re-identify after starting TLS
-    server.login(sender_email, password)  # Log in to your Gmail account
-    server.sendmail(sender_email, receiver_email, message)  # Send the email
-    print("Email sent successfully!")
-# Note: Make sure to enable "Less secure app access" in your Google account settings if you're not using 2FA.
+try:
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo()  # Can be used to identify the client to the server
+        server.starttls(context=context)  # Secure the connection using TLS
+        server.ehlo()  # Re-identify after starting TLS
+        server.login(sender_email, password)  # Log in to your Gmail account
+        server.sendmail(sender_email, receiver_email, message)  # Send the email
+        print("Email sent successfully!")
+except Exception as e:
+    print(f"An error occurred: {e}")
