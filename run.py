33c5aa820cd1b322e75d8ba5c94f57e0957f9ad6
Châu Minh Quân <104240214@student.vgu.edu.vn>
2025-04-13 23:34:13 +0700

    [Update]: little spacing in run.py

--- run.py
+++ run.py
@@ -1,17 +1,18 @@
 if __name__ == '__main__':
     from dotenv import load_dotenv
-    load_dotenv()
     from run_flask import run_flask
     from run_ws import run_ws
     from threading import Thread
     import time
     import logging
-    import os
+    load_dotenv()
     logging.basicConfig(level=logging.INFO)
     logging.info("Starting Flask server...")
     flask_thread = Thread(target=run_flask)
     flask_thread.start()
+    
     time.sleep(5)  # Give Flask some time to start
+    
     logging.info("Starting WebSocket server...")
     ws_thread = Thread(target=run_ws)
     ws_thread.start()
