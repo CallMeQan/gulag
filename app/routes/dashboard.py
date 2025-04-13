963241e1822b7c48643ae3caaf81ced1530c6de6
ceenen <phunh.work@gmail.com>
2025-04-14 03:25:56 +0700

    Added maps to dashboard/map

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, jsonify
 from flask import render_template, redirect, url_for, session, request
 from flask_login import login_user, logout_user, login_required
+
 from dotenv import load_dotenv
 from os import getenv
 import requests
+from json import dumps, loads
 
 from ..models import User
 from ..extensions import db, login_manager
@@ -23,11 +25,49 @@ def load_user(user_id):
 # Main
 @dashboard_bp.route("/map", methods = ["GET", "POST"])
 def map():
-    google_map_api_key = getenv("GOOGLE_MAP_API_KEY")
-    get_iot_url = getenv("GET_IOT_URL")
-    response = requests.get(get_iot_url)
-    data = response.json()
-    print(data["data"])
-    print(data["user_id"])
+    # get_iot_url = getenv("GET_IOT_URL")
+    # response = requests.get(get_iot_url)
+    # data = response.json()
+    # print(data["data"])
+    # print(data["user_id"])
     
-    return render_template("dashboard/map.html", gg_api = google_map_api_key)
+    # Current position (mock data)
+    geojson = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "properties": {
+                    "name": "Running Route",
+                    "type": "Loop Run"
+                },
+                "geometry": {
+                "type": "LineString",
+                    "coordinates": [
+                    [106.612543, 11.107656],
+                    [106.612943, 11.107656],
+                    [106.613543, 11.107656],
+                    [106.614543, 11.107656],
+                    [106.615143, 11.107256],
+                    [106.616143, 11.107456],
+                    [106.617543, 11.107356],
+                    [106.614543, 11.107256],
+                    [106.614243, 11.107156],
+                    [106.613243, 11.107056]
+                ]
+            }
+        }
+    ]
+    }
+    current_pos = [11.107656, 106.612543]
+
+    current_pos = dumps(current_pos)
+    loaded_pos = loads(current_pos)
+
+    # Parse dictionary into json-string
+    geojson = dumps(geojson)
+    loaded_geojson = loads(geojson)
+
+    # Get map api key
+    map_key = getenv("MAP_KEY")
+    return render_template("dashboard/map.html", map_key = map_key, current_pos = loaded_pos, geojson = loaded_geojson)
