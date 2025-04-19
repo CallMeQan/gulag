3cc66813a105ce113de5f7a8326d75e0a4a75202
Pham Trong Quy <104240232@student.vgu.edu.vn>
2025-04-19 18:47:06 +0700

    Format FE

--- app/routes/home.py
+++ app/routes/home.py
@@ -20,6 +20,10 @@ def index():
 def homepage():
     return render_template('home/homepage.html')
 
+@home_bp.route('/about')
+def about():
+    return render_template('home/about_us.html')
+
 @home_bp.route('/set-goal', methods = ["GET", "POST"])
 def set_goal():
     if request.method == "POST":
